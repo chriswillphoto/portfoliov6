b436935e6e5f11c17d9494d9fedcb58ef74d2452
chriswillphoto <christophernekich@gmail.com>
2018-07-26 22:34:41 +1000

    structure changes and titles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,36 @@
-const $$ = (selector, el = D) => {
-  el.querySelectorAll(selector)
-}
+(function() {
+  const $$ = (selector, el = D) => {
+    el.querySelectorAll(selector);
+  };
 
-let viewBox = document.querySelector('#viewbox');
-let navButtons = document.querySelectorAll('.nav-button')
-let windowWidth = window.innerWidth;
 
-const shiftView = function(e){
-  const newLeft = e.target.getAttribute('data-left');
-  e.preventDefault();
-  viewBox.style.left = newLeft;
-  console.log(newLeft)
-}
+  let viewBox = document.querySelector("#viewbox");
+  let navButtons = document.querySelectorAll(".nav-button");
+  let windowWidth = window.innerWidth;
 
-navButtons.forEach(function(el){
-  el.addEventListener('click', shiftView)
-})
-for(let i=0; i < navButtons.length; i++){
-  navButtons[i].setAttribute('data-test', i);
-  navButtons[i].addEventListener('click', function(){
-    console.log(this.innerText);
-  })
-}
+  viewBox.style.left = 0;
+
+  const shiftView = function(e) {
+    const newLeft = e.target.getAttribute("data-left");
+    e.preventDefault();
+    viewBox.style.left = newLeft;
+    console.log(newLeft);
+  };
+
+  navButtons.forEach(function(el) {
+    el.addEventListener("click", shiftView);
+  });
+
+  // event delegation
+  document.addEventListener('click', function (event) {
+
+    if (event.target.matches('.modal-open')) {
+      // Run your code to open a modal
+    }
+  
+    if (event.target.matches('.close')) {
+      // Run your code to close a modal
+    }
+  
+  }, false);
+})();
