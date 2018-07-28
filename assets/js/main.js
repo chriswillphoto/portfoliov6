7ad63600260076850eead94845e93563e71406c5
chriswillphoto <christophernekich@gmail.com>
2018-07-29 04:59:52 +1000

    added drag handle and icon library

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,51 @@
 (function() {
-  const $$ = (selector, el = D) => {
-    el.querySelectorAll(selector);
-  };
-
-
   let viewBox = document.querySelector("#viewbox");
   let navButtons = document.querySelectorAll(".nav-button");
   let windowWidth = window.innerWidth;
+  let draggables = document.querySelectorAll(".draggable");
+
+  draggables.forEach(function(el){
+    const dragHandle = document.createElement('div');
+    dragHandle.innerHTML = "<i class='icon ion-ios-move'></i>"
+    dragHandle.classList = "drag-handle";
+    el.appendChild(dragHandle)
+  })
+
+  let whatever = document.querySelectorAll(".home > *");
+
+  console.log(whatever);
+  let elHeights;
+  whatever.forEach(function(i) {
+    elHeights += i.clientHeight;
+  });
 
   viewBox.style.left = 0;
 
   const shiftView = function(e) {
     const newLeft = e.target.getAttribute("data-left");
     e.preventDefault();
-    viewBox.style.left = newLeft;
-    console.log(newLeft);
+    if (viewBox.style.left != newLeft) {
+      viewBox.style.left = newLeft;
+      console.log(newLeft);
+    }
   };
 
-  navButtons.forEach(function(el) {
-    el.addEventListener("click", shiftView);
-  });
-
   // event delegation
-  document.addEventListener('click', function (event) {
+  document.addEventListener(
+    "click",
+    function(event) {
+      if (event.target.matches(".nav-button")) {
+        shiftView(event);
+      }
 
-    if (event.target.matches('.modal-open')) {
-      // Run your code to open a modal
-    }
-  
-    if (event.target.matches('.close')) {
-      // Run your code to close a modal
-    }
-  
-  }, false);
+      if (event.target.matches(".modal-open")) {
+        // Run your code to open a modal
+      }
+
+      if (event.target.matches(".close")) {
+        // Run your code to close a modal
+      }
+    },
+    false
+  );
 })();
