a29842462ad106f78f3dc0cc29d79a265b6f68dc
chriswillphoto <christophernekich@gmail.com>
2018-09-03 22:36:40 +1000

    added project descriptions and project modal functionality

---
+++ app/js/work.js
@@ -0,0 +1,142 @@
+// const effthegym = document.querySelector("#effthegym");
+const projectModal = document.querySelector(".project-modal .content");
+const effthegymDesc =
+  '<div class="words"><h2>Eff The Gym</h2><p>Eff The Gym is a first attempt at a Progressive Web App (PWA). Built in React and Redux, the aim of this app is to provide a simple  means to tracking workouts in the gym. Users create workouts, fill them with exercises and then cycle through them one at a time, either by sets  or by time, until the workout is complete. Designed for mobile, because not too many people take their laptops to the gym. Currently a work in progress. </p><div class="tech">  <div class="stack">    <h5>Tech Stack</h5>    <p>React</p>    <p>Google Firebase</p>  </div>  <div class="lib">    <h5>Libraries</h5>    <p>Redux</p>    <p>firebase</p>  </div></div><a href="https://effthegym.firebaseapp.com/#/" target="_blank">Live Site</a><a href="https://github.com/chriswillphoto/effthegym" target="_blank">Github Repo</a></div>';
+const soundbarDesc = `<div class="words">
+<h2>Soundbar</h2>
+<p>SoundBar is a noisemaking toy. Various sampled sounds, each paired to a different animation and key, make for a synesthetic in-browser beat pad.</p>
+<div class="tech">
+  <div class="stack">
+    <h5>Tech Stack</h5>
+    <p>Static Website - HTML/CSS/JS</p>
+  </div>
+  <div class="lib">
+    <h5>Libraries</h5>
+    <p>jQuery</p>
+    <p>Howler.js</p>
+    <p>Paper.js</p>
+  </div>
+</div><!-- tech -->
+<a href="https://chriswillphoto.github.io/soundbar/" target="_blank">Live Site</a>
+<a href="https://github.com/chriswillphoto/soundbar" target="_blank">Github Repo</a>
+</div>`;
+
+const hotboxDesc = `<div class="words">
+<h2>Hotbox</h2>
+<p>HotBox is a social media platform built to support a ubiquitous consumer product, in this case; toasters. Users can view base model toasters, and custom toasters that have been added to the database. Once signed up for an account, users can rate and review base model toasters, add their own customised versions to their account, comment on the custom versions of others and upvote and/or bookmark their favourites.</p>
+<div class="tech">
+  <div class="stack">
+    <h5>Tech Stack</h5>
+    <p>Ruby-on-Rails</p>
+  </div>
+  <div class="lib">
+    <h5>Libraries</h5>
+    <p>Bootstrap</p>
+    <p>jQuery</p>
+  </div>
+</div> <!-- tech -->
+<a href="http://hotboxing.herokuapp.com" target="_blank">Live Site</a>
+<a href="https://github.com/chriswillphoto/hotbox" target="_blank">Github Repo</a>
+</div>`;
+
+const grumbleDesc = `<div class="words">
+<h2>Grumble</h2>
+<p>Grumble is a food app for the indecisive. Users enter a suburb and restaurants in that suburb are presented one by one for users to flick through. Once signed up 
+for an account, users can add restaurants to their shortlist or their favourites list, and view restaurants in further detail to help decide where to eat.</p>
+<p>Grumble was created in collaboration with two other developers as a learning experience in a group setting. The front end is a react app that works with a ruby-on-rails 
+back end through the means of ajax requests. User authentication is done through the use of JSON Web Tokens.</p>
+<div class="tech">
+  <div class="stack">
+    <h5>Tech Stack</h5>
+    <p>React</p>
+    <p>Ruby-on-Rails</p>
+  </div>
+  <div class="lib">
+    <h5>Libraries</h5>
+    <p>axios</p>
+    <p>Underscore.js</p>
+  </div>
+</div> <!-- tech -->
+<a href="https://chriswillphoto.github.io/grumble/" target="_blank">Live Site</a>
+<a href="https://github.com/chriswillphoto/grumble" target="_blank">Github Repo</a>
+</div>`;
+
+const quotDesc = `<div class="words">
+<h2>&amp;quot</h2>
+<p>Quot is a quote reference app built in Vue.js, which utilises ajax requests to pull quotes from a ruby-on-rails API. Users can pick a topic (motivation, creativity, love, etc) and randomly cycle through related quotes. Alternatively, users can look at all the quotes in the database and filter them down through the use of the search bar. </p>
+<div class="tech">
+  <div class="stack">
+    <h5>Tech Stack</h5>
+    <p>Vue.js</p>
+    <p>Ruby-on-Rails</p>
+  </div>
+  <div class="lib">
+    <h5>Libraries</h5>
+    <p>vue router</p>
+    <p>axios</p>
+  </div>
+</div> <!-- tech -->
+<a href="https://chriswillphoto.github.io/quot-app/" target="_blank">Live Site</a>
+<a href="https://github.com/chriswillphoto/quot-app" target="_blank">Github Repo</a>
+</div>`;
+
+const scrillDesc = `<div class="words">
+<h2>Scrilla</h2>
+<p>Scrilla is a budget visualisation tool built in React. Input budget items, their rate of occurence and their amounts to see them amalgamated and displayed by Month, Fortnight or Week in pie or bar chart form.</p>
+<div class="tech">
+  <div class="stack">
+    <h5>Tech Stack</h5>
+    <p>React</p>
+  </div>
+  <div class="lib">
+    <h5>Libraries</h5>
+    <p>React Router</p>
+    <p>chart.js</p>
+    <p>randomcolor</p>
+  </div>
+</div> <!-- tech -->
+<a href="https://chriswillphoto.github.io/scrilla" target="_blank">Live Site</a>
+<a href="https://github.com/chriswillphoto/scrilla" target="_blank">Github Repo</a>
+</div>`;
+
+const pixelDeli = `<div class="words">
+<h2>Pixel Deli</h2>
+<p>Pixel Deli is a collaborative art project, much like r/place. As users sign in, they are placed in a queue, with the first in line able to pick a color from the rainbow and place it
+down on the 'canvas' (10000 divs). They are then given the option to rejoin the back of the queue. This creates an opportunity for an ever changing communal art piece, bringing people together in the act
+of creation.</p>
+<div class="tech">
+  <div class="stack">
+    <h5>Tech Stack</h5>
+    <p>Ruby-on-Rails</p>
+  </div>
+  <div class="lib">
+    <h5>Libraries</h5>
+    <p>jQuery</p>
+  </div>
+</div> <!-- tech -->
+<a href="https://pixeldeli.herokuapp.com" target="_blank">Live Site</a>
+<a href="https://github.com/chriswillphoto/everybodypaint" target="_blank">Github Repo</a>
+</div>`;
+
+const projectDescriptions = [
+  effthegymDesc,
+  soundbarDesc,
+  hotboxDesc,
+  grumbleDesc,
+  quotDesc,
+  scrillDesc,
+  pixelDeli
+];
+
+const projectButtons = document.querySelectorAll(".project-button");
+projectButtons.forEach(function(e, i) {
+  e.setAttribute("data-desc-order", i);
+});
+
+document.addEventListener("click", function(event) {
+  if (event.target.matches(".project-button")) {
+    let orderNum = event.target.getAttribute("data-desc-order");
+
+    projectModal.innerHTML = projectDescriptions[orderNum];
+  }
+});
