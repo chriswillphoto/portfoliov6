25bc49993b47f83436a22004383e78fd2d27b0b0
chriswillphoto <christophernekich@gmail.com>
2018-09-09 14:47:42 +1000

    added images to work desc

--- app/js/work.js
+++ app/js/work.js
@@ -1,7 +1,10 @@
 // const effthegym = document.querySelector("#effthegym");
 const projectModal = document.querySelector(".project-modal .content");
 const effthegymDesc =
-  '<div class="words"><h2>Eff The Gym</h2><p>Eff The Gym is a first attempt at a Progressive Web App (PWA). Built in React and Redux, the aim of this app is to provide a simple  means to tracking workouts in the gym. Users create workouts, fill them with exercises and then cycle through them one at a time, either by sets  or by time, until the workout is complete. Designed for mobile, because not too many people take their laptops to the gym. Currently a work in progress. </p><div class="tech">  <div class="stack">    <h5>Tech Stack</h5>    <p>React</p>    <p>Google Firebase</p>  </div>  <div class="lib">    <h5>Libraries</h5>    <p>Redux</p>    <p>firebase</p>  </div></div><div class="link-box"><a href="https://effthegym.firebaseapp.com/#/" target="_blank">Live Site</a><a href="https://github.com/chriswillphoto/effthegym" target="_blank">Github Repo</a></div></div>';
+  `<div class="words"><h2>Eff The Gym</h2>
+  
+<img class="image" src='https://res.cloudinary.com/dyqesnour/image/upload/q_60/v1518951721/effthegym_vnd3tn.jpg' />
+  <p>Eff The Gym is a first attempt at a Progressive Web App (PWA). Built in React and Redux, the aim of this app is to provide a simple  means to tracking workouts in the gym. Users create workouts, fill them with exercises and then cycle through them one at a time, either by sets  or by time, until the workout is complete. Designed for mobile, because not too many people take their laptops to the gym. Currently a work in progress. </p><div class="tech">  <div class="stack">    <h5>Tech Stack</h5>    <p>React</p>    <p>Google Firebase</p>  </div>  <div class="lib">    <h5>Libraries</h5>    <p>Redux</p>    <p>firebase</p>  </div></div><div class="link-box"><a href="https://effthegym.firebaseapp.com/#/" target="_blank">Live Site</a><a href="https://github.com/chriswillphoto/effthegym" target="_blank">Github Repo</a></div></div>`;
 const soundbarDesc = `<div class="words">
 <h2>Soundbar</h2>
 <img class="image" src='https://res.cloudinary.com/dyqesnour/image/upload/c_scale,q_60,w_800/v1517654901/Screen_Shot_2018-01-06_at_12.41.56_pm_le3axp.png' />
@@ -26,6 +29,7 @@ const soundbarDesc = `<div class="words">
 
 const hotboxDesc = `<div class="words">
 <h2>Hotbox</h2>
+<img class="image" src='https://res.cloudinary.com/dyqesnour/image/upload/c_scale,q_60,w_800/v1517629840/Screen_Shot_2018-02-03_at_2.49.49_pm_ugrox4.png' />
 <p>HotBox is a social media platform built to support a ubiquitous consumer product, in this case; toasters. Users can view base model toasters, and custom toasters that have been added to the database. Once signed up for an account, users can rate and review base model toasters, add their own customised versions to their account, comment on the custom versions of others and upvote and/or bookmark their favourites.</p>
 <div class="tech">
   <div class="stack">
@@ -47,6 +51,7 @@ const hotboxDesc = `<div class="words">
 
 const grumbleDesc = `<div class="words">
 <h2>Grumble</h2>
+<img class="image" src='https://res.cloudinary.com/dyqesnour/image/upload/c_scale,q_60,w_800/v1517654579/Screen_Shot_2018-02-03_at_9.42.05_pm_cumd9p.png' />
 <p>Grumble is a food app for the indecisive. Users enter a suburb and restaurants in that suburb are presented one by one for users to flick through. Once signed up 
 for an account, users can add restaurants to their shortlist or their favourites list, and view restaurants in further detail to help decide where to eat.</p>
 <p>Grumble was created in collaboration with two other developers as a learning experience in a group setting. The front end is a react app that works with a ruby-on-rails 
@@ -72,6 +77,7 @@ back end through the means of ajax requests. User authentication is done through
 
 const quotDesc = `<div class="words">
 <h2>&amp;quot</h2>
+<img class="image" src='https://res.cloudinary.com/dyqesnour/image/upload/c_scale,q_60,w_800/v1517654266/Screen_Shot_2018-02-03_at_9.36.52_pm_fn8dks.png' />
 <p>Quot is a quote reference app built in Vue.js, which utilises ajax requests to pull quotes from a ruby-on-rails API. Users can pick a topic (motivation, creativity, love, etc) and randomly cycle through related quotes. Alternatively, users can look at all the quotes in the database and filter them down through the use of the search bar. </p>
 <div class="tech">
   <div class="stack">
@@ -94,6 +100,7 @@ const quotDesc = `<div class="words">
 
 const scrillDesc = `<div class="words">
 <h2>Scrilla</h2>
+<img class="image" src='https://res.cloudinary.com/dyqesnour/image/upload/c_scale,q_60,w_800/v1517654033/Screen_Shot_2018-02-03_at_9.33.06_pm_emlnwk.png' />
 <p>Scrilla is a budget visualisation tool built in React. Input budget items, their rate of occurence and their amounts to see them amalgamated and displayed by Month, Fortnight or Week in pie or bar chart form.</p>
 <div class="tech">
   <div class="stack">
@@ -115,6 +122,7 @@ const scrillDesc = `<div class="words">
 
 const pixelDeli = `<div class="words">
 <h2>Pixel Deli</h2>
+<img class="image" src='https://res.cloudinary.com/dyqesnour/image/upload/c_scale,q_60,w_800/v1517653749/Screen_Shot_2018-02-03_at_9.28.14_pm_evh9oo.png' />
 <p>Pixel Deli is a collaborative art project, much like r/place. As users sign in, they are placed in a queue, with the first in line able to pick a color from the rainbow and place it
 down on the 'canvas' (10000 divs). They are then given the option to rejoin the back of the queue. This creates an opportunity for an ever changing communal art piece, bringing people together in the act
 of creation.</p>
